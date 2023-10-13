e58659c1d434f482342996298d28d81061b053e0
Ansel Chang <ansel.chang@yahoo.com>
2023-10-13 03:31:30 -0400

    start work on svg

---
+++ src/app/components/svg/svg.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/*
+Handles the entire playable area as an SVG element. Uses svg-pan-zoom library
+to handle zooming and panning the playfield.
+*/
+
+@Component({
+  selector: 'app-svg',
+  templateUrl: './svg.component.html',
+  styleUrls: ['./svg.component.scss']
+})
+export class SvgComponent {
+
+}
