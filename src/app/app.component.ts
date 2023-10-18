933efdb9f4ec2ff0c914cdb251e4a91c56628011
Ansel Chang <ansel.chang@yahoo.com>
2023-10-18 03:48:58 -0500

    disable all mouse interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { Socket, io } from 'socket.io-client';
 import { FormsModule } from '@angular/forms';
 import { SocketService } from './services/socket.service';
 import { GameStateService } from './services/game-state.service';
 import { Game } from './models/game';
+import { Player } from './models/player';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,15 @@ import { Game } from './models/game';
 export class AppComponent implements OnInit, OnDestroy {
   title = 'multiplayer';
 
-  constructor(private gameStateService: GameStateService, private socketService: SocketService) {}
+  constructor(private gameStateService: GameStateService,
+    private socketService: SocketService,
+    private ngZone: NgZone) {}
 
   ngOnInit(): void {
     this.socketService.initSocket();
+    this.ngZone.runOutsideAngular(() => {
+      this.addEventListeners();
+    });
   }
 
   public isConnectedToServer(): boolean {
@@ -27,8 +33,42 @@ export class AppComponent implements OnInit, OnDestroy {
     return this.gameStateService.getGame();
   }
 
+  public getMyself(): Player {
+    return this.getGame().getMe();
+  }
+
   ngOnDestroy(): void {
     this.socketService.destroySocket();
+
+    document.removeEventListener('wheel', this.preventZoom);
+    document.removeEventListener('touchmove', this.preventScroll);
+    window.removeEventListener('scroll', this.preventElasticScroll);
   }
+
+  addEventListeners() {
+    // Disable pinch zooming
+    document.addEventListener('wheel', this.preventZoom, { passive: false });
+
+    // Disable scrolling using touch
+    document.addEventListener('touchmove', this.preventScroll, { passive: false });
+
+    // Disable elastic scrolling on macOS
+    window.addEventListener('scroll', this.preventElasticScroll);
+  }
+
+  preventZoom(e: WheelEvent) {
+    if (e.ctrlKey) {
+      e.preventDefault();
+    }
+  }
+
+  preventScroll(e: TouchEvent) {
+    e.preventDefault();
+  }
+
+  preventElasticScroll() {
+    window.scrollTo(0, 0);
+  }
+
 
 }
--- src/app/components/svg/svg.component.ts
+++ src/app/components/svg/svg.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input } from '@angular/core';
 import { Game } from 'src/app/models/game';
 import { Player } from 'src/app/models/player';
 
@@ -12,11 +12,19 @@ to handle zooming and panning the playfield.
   templateUrl: './svg.component.html',
   styleUrls: ['./svg.component.scss']
 })
-export class SvgComponent {
+export class SvgComponent implements AfterViewInit {
   @Input() game!: Game;
+  @Input() myself!: Player;
 
+  private WINDOW_WIDTH!: number;
+  private WINDOW_HEIGHT!: number;
 
-  constructor() { }
+  constructor(private elRef: ElementRef) { }
+
+  ngAfterViewInit() {
+    this.updateWindowDimensions();
+  }
+
 
   public getGameWidth(): number {
     return this.game.getGameWidth();
@@ -29,6 +37,28 @@ export class SvgComponent {
   public getPlayers(): Player[] {
     return this.game.getPlayers();
   }
+
+  @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+        this.updateWindowDimensions();
+    }
+
+  updateWindowDimensions() {
+    this.WINDOW_WIDTH = this.elRef.nativeElement.clientWidth;
+    this.WINDOW_HEIGHT = this.elRef.nativeElement.clientHeight;
+    console.log(`Window dimensions: ${this.WINDOW_WIDTH} x ${this.WINDOW_HEIGHT}`);
+    
+}
+
+  public getTransform(): string {
+
+    const offsetX = (this.WINDOW_WIDTH / 2) - this.myself.x;
+    const offsetY = (this.WINDOW_HEIGHT / 2) - this.myself.y;
+    const zoomLevel = 1;
+
+    return `translate(${0}, ${0}) scale(${zoomLevel})`;
+
+  };
   
 
 }
